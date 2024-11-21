a362a554366d61edccffa2611c01e231f2f1a639
Vilhelm Persson <vilhelm.persson@student.uclouvain.be>
2024-11-21 18:33:22 +0100

    made plots from the csv file

--- project/server_implementation/run_plots3.py
+++ project/server_implementation/run_plots3.py
@@ -10,43 +10,43 @@ plt.tight_layout()
 
 results = pd.read_csv("performance_data.csv")
 
-case1_x_pre = []
-case1_y_pre = []
-case1_x_aft = []
-case1_y_aft = []
+def plot_case(name, n1, n2, pre_ledger, aft_ledger):
 
-for index, row in results.iterrows():
-    if index % 6 == 0:
-        case1_x_pre.append(row['make_cmd'])
-        case1_y_pre.append(row['time elapsed'])
-    elif index % 6 == 1:
-        case1_x_aft.append(row['make_cmd'])
-        case1_y_aft.append(row['time elapsed'])
+    case_y_pre = []
+    case_y_aft = []
 
+    for index, row in results.iterrows():
+        if index % 6 == n1:
+            case_y_pre.append(row['transfers_per_sec'])
+        elif index % 6 == n2:
+            case_y_aft.append(row['transfers_per_sec'])
 
-a = [2, 2, 2, 2]
-b = [1, 1, 1, 1]
+    barWidth = 0.25
+    fig = plt.subplots(figsize =(12, 8)) 
 
-barWidth = 0.25
-fig = plt.subplots(figsize =(12, 8)) 
+    br1 = np.arange(len(case_y_pre)) 
+    br2 = [x + barWidth for x in br1]  
 
-br1 = np.arange(len(case1_y_pre)) 
-br2 = [x + barWidth for x in br1]  
+    plt.bar(br1, case_y_pre, color ='r', width = barWidth, 
+            edgecolor ='grey', label = pre_ledger) 
 
-plt.bar(br1, case1_y_pre, color ='r', width = barWidth, 
-        edgecolor ='grey', label ='Smaller matrix size') 
+    plt.bar(br2, case_y_aft, color ='g', width = barWidth, 
+            edgecolor ='grey', label = aft_ledger) 
 
-plt.bar(br2, case1_y_aft, color ='g', width = barWidth, 
-        edgecolor ='grey', label ='Bigger matrix size') 
+    #plt.ylim(0)
 
-plt.ylim(0)
+    plt.yscale("log")
 
-plt.xlabel('Compile flags', fontweight ='bold', fontsize = 15) 
-plt.ylabel('Transfers per second', fontweight ='bold', fontsize = 15) 
-plt.xticks([r + barWidth for r in range(len(case1_y_pre))], 
-        ['DUNROLL', 'DCACHE AWARE', 'DBEST', 'DUNROLL and DCACHE AWARE'])
+    plt.xlabel('Compile flags', fontweight ='bold', fontsize = 15) 
+    plt.ylabel('Transfers per second', fontweight ='bold', fontsize = 15) 
+    plt.xticks([r + barWidth for r in range(len(case_y_pre))], 
+            ['DUNROLL', 'DCACHE AWARE', 'DBEST', 'DUNROLL and DCACHE AWARE'])
 
-plt.legend()
-plt.savefig('measurements/case1.svg', format='svg')
-plt.title('case 1')
-plt.show()
+    plt.legend()
+    plt.savefig('measurements/case1.svg', format='svg')
+    plt.title('name')
+    plt.show()
+
+#plot_case('case 1', 0, 1, 'Smaller matrix size', 'Bigger matrix size')
+#plot_case('case 2', 2, 3, 'Smaller pattern size', 'Bigger pattern size')
+plot_case('case 3', 4, 5, 'Smaller amount of patterns', 'Larger amount of patterns')
