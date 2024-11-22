396405fdc8879d1d6429674963ac10dcf4ad7865
Ji <ji-liang.leungwing@student.uclouvain.be>
2024-11-22 16:48:20 +0100

    csv data

--- project/server_implementation/run_perf3.py
+++ project/server_implementation/run_perf3.py
@@ -51,8 +51,8 @@ def generate_make_config():
         cmds.append(f"perf stat --timeout 30010 -o output.txt -e cache-misses,cache-references make -B run_release CFLAGS+='{flag}'")
 
     # try for different worker counts
-    for i in range(2, 11): 
-        cmds.append(f"perf stat --timeout 30010 -o output.txt -e cache-misses,cache-references make -B run_release CFLAGS+=-DBEST NB_WORKER={i}")
+    #for i in range(2, 11): 
+    #    cmds.append(f"perf stat --timeout 30010 -o output.txt -e cache-misses,cache-references make -B run_release CFLAGS+=-DBEST NB_WORKER={i}")
 
     return cmds
 
