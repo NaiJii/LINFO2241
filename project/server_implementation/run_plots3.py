ae0452d63e68b37c66c057f4b988775db49232f4
Vilhelm Persson <vilhelm.persson@student.uclouvain.be>
2024-11-23 17:39:06 +0100

    updated graphs plot

--- project/server_implementation/run_plots3.py
+++ project/server_implementation/run_plots3.py
@@ -10,7 +10,13 @@ plt.tight_layout()
 
 results = pd.read_csv("performance_data.csv")
 
-def plot_case(name, n1, n2, pre_ledger, aft_ledger):
+for index, row in results.iterrows():
+    if (index % 6) == 0:
+        print('----------------------')
+    print(round((float(row['cache-misses'].replace(",","")) / float(row['cache-references'].replace(",","")) * 100), 2))
+
+
+def plot_case(name, title, n1, n2, pre_ledger, aft_ledger):
 
     case_y_pre = []
     case_y_aft = []
@@ -20,6 +26,7 @@ def plot_case(name, n1, n2, pre_ledger, aft_ledger):
             case_y_pre.append(row['transfers_per_sec'])
         elif index % 6 == n2:
             case_y_aft.append(row['transfers_per_sec'])
+
 
     barWidth = 0.25
     fig = plt.subplots(figsize =(12, 8)) 
@@ -37,15 +44,15 @@ def plot_case(name, n1, n2, pre_ledger, aft_ledger):
 
     plt.yscale("log")
 
-    plt.xlabel('Compile flags', fontweight ='bold', fontsize = 20) 
-    plt.ylabel('Transfers per second', fontweight ='bold', fontsize = 20) 
+    plt.xlabel('Compile flags', fontweight ='bold', fontsize = 22) 
+    plt.ylabel('Transfers per second', fontweight ='bold', fontsize = 22) 
     plt.xticks([r + (barWidth/2) for r in range(len(case_y_pre))], 
-            ['ORIGINAL', 'DUNROLL', 'DCACHE AWARE', 'DBEST', 'DUNROLL & DCACHE AWARE'], fontsize=14)
+            ['NONE', 'UNROLL', 'CACHE AWARE', 'BEST', 'UNROLL & CACHE AWARE'], fontsize=16)
 
-    #plt.legend()
-    plt.title('Case ' + name, fontweight = 'bold', fontsize = 25)
+    plt.legend(fontsize = 16)
+    plt.title(title, fontweight = 'bold', fontsize = 30)
     plt.savefig('measurements/case_' + name + '.svg', format='svg')
 
-plot_case('1', 0, 1, 'Smaller matrix size', 'Bigger matrix size')
-plot_case('2', 2, 3, 'Smaller pattern size', 'Bigger pattern size')
-plot_case('3', 4, 5, 'Smaller amount of patterns', 'Larger amount of patterns')
+plot_case('1', 'Matrix size test', 0, 1, 'Smaller matrix size', 'Bigger matrix size')
+plot_case('2', 'Pattern size test', 2, 3, 'Smaller pattern size', 'Bigger pattern size')
+plot_case('3', 'Number of patterns test', 4, 5, 'Smaller amount of patterns', 'Larger amount of patterns')
